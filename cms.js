19b0fbdd5dcb10cc8d8e3d6403d80e93867263eb
Kyle Pingue <pinguek@gmail.com>
2020-07-02 14:49:13 -0500

    changed the view departments and roles without repetition, need to work on updating employee roles

--- cms.js
+++ cms.js
@@ -78,20 +78,28 @@ function viewDepts(){
 
     connection.query("SELECT * FROM department", (err, data) =>{
         if (err) throw err;
-        var diffDept = data[0].name
-        var deptArray = [diffDept]
+
         
-        for(var i=0; i<data.length; i++){
-            if(diffDept !== data[i].name){
+        var deptArray = [data[0].name];
+
+        console.log("Data", data);
+        for(var i = 0; i<data.length; i++){
+            
+            if (deptArray.indexOf(data[i].name)> -1) {
+                console.log(data[i].name, " is already here");
+                
+            }
+            else{
+                console.log(data[i].name, "is NOT already here")
                 deptArray.push(data[i].name)
-            }    
+            }
+            
         }
-        for(var i=0; i<deptArray.length; i++){
-            console.table(deptArray[i]);
-        }
-        // console.table(data)
+
+        console.table(deptArray);
+
         initalise();
-        // connection.end
+        
     })
 };
 
@@ -100,7 +108,26 @@ function viewRoles(){
 
     connection.query("SELECT * FROM role", (err, data) =>{
         if (err) throw err;
-        console.table(data)
+
+        
+        var rolesArray = [data[0].title];
+
+        console.log("Data", data);
+        for(var i = 0; i<data.length; i++){
+            
+            if (rolesArray.indexOf(data[i].title)> -1) {
+                console.log(data[i].title, " is already here");
+                
+            }
+            else{
+                console.log(data[i].title, "is NOT already here")
+                rolesArray.push(data[i].title)
+            }
+            
+        }
+
+        console.table(rolesArray);
+
         initalise();
         connection.end
     })
@@ -245,54 +272,152 @@ function setRole(newEmployee, dept){
                 initalise();
             })
             break;
+
+        //Define Finance roles
+        case("Finance"):
+        connection.query(`INSERT INTO department(name) 
+        Values ("Finance")`, (err, data) =>{
+            if (err) throw err;
+                            });
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "financeRoles",
+                message: "What Finance role?",
+                choices: ["Finance Lead", "Accountant"]
+            }
+        ]).then(response =>{
+            switch(response.financeRoles){
+                case("Finance Lead"):
+                newEmployee.employeeTitle = response.financeRoles;
+                newEmployee.employeeSalary = 145000;
+                connection.query(`INSERT INTO role(title, salary) 
+                Values ("Finance Lead", "145000")`, (err, data) =>{
+                    if (err) throw err;
+                                    });
+                break;
+                
+                case("Accountant"):
+                employeeTitle= response.financeRoles;
+                employeeSalary = 125000;
+                connection.query(`INSERT INTO role(title, salary) 
+                Values ("Accountant", "125000")`, (err, data) =>{
+                    if (err) throw err;
+                                    });
+                break;
             
-     }
-    // initalise(); - DOES NOT WORK!
-}
+                }
+                initalise();
+            })
+            break;
+
+        //Define Legal roles
+        case("Legal"):
+        connection.query(`INSERT INTO department(name) 
+        Values ("Legal")`, (err, data) =>{
+            if (err) throw err;
+                            });
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "legalRoles",
+                message: "What Legal role?",
+                choices: ["Legal Team Lead", "Lawyer"]
+            }
+        ]).then(response =>{
+            switch(response.legalRoles){
+                case("Legal Team Lead"):
+                newEmployee.employeeTitle = response.legalRoles;
+                newEmployee.employeeSalary = 250000;
+                connection.query(`INSERT INTO role(title, salary) 
+                Values ("Legal Team Lead", "250000")`, (err, data) =>{
+                    if (err) throw err;
+                                    });
+                break;
+                
+                case("Lawyer"):
+                employeeTitle= response.legalRoles;
+                employeeSalary = 190000;
+                connection.query(`INSERT INTO role(title, salary) 
+                Values ("Lawyer", "190000")`, (err, data) =>{
+                    if (err) throw err;
+                                    });
+                break;
+            
+                }
+                initalise();
+            })
+            break;
+            
+     }//End of Switch(dept)
+} //End of Function
  
 //Function to UPDATE a current EMPLOYEE
-// var array = ["Mark", "John", "Peter", "Paul"]
+
 var array = [];
 
 function updateEmployee(array){
 
 var chosenName;
-    // console.log("Array is", array)     //To see if the array passed
+    
     getEmployee(array)
     //Function to get the list of names
     function getEmployee(){
         var employeeNames = connection.query(`SELECT employee.id, first_name, last_name FROM employee`, (err, data) =>{
             if (err) throw err;
             for(i=0; i < data.length; i++){
-                var fullname = `${data[i].first_name} ${data[i].last_name}`
+                var fullname = `${data[i].id}, ${data[i].first_name} ${data[i].last_name}`
                 array.push(fullname)
             }
-            // console.log("Data", data);
-            // console.log("Data Length", data.length);
-            // console.log("Full Names", array)
-            inquirer.prompt([           //To see if the array passed
+            inquirer.prompt([           
         {
             type:"list",
-            name:"employeeUpdateChoice",
+            name:"employeeUpdateName",
             message: "Which employee would you like to update?",
             choices: array   
         },
         {
             type:"list",
-            name:"employeeUpdateRole",
+            name:"employeeUpdateDept",
             message: "What department would you now like to select?",
-            choices:  []
-        }
+            choices:  ["Engineering",
+                    "Sales",
+                    "Finance",
+                    "Legal"]
+        },
+        {
+            type:"list",
+            name:"employeeUpdateRole",
+            message: "What role would you now like to assign?",
+            choices:  ["Lead Engineer",
+                        "Software Engineer",
+                        "Sales Lead",
+                        "Salesperson",
+                        "Legal Team Lead",
+                        "Lawyer",
+                        "Finance Lead",
+                        "Accountant"]
+        },
     ]).then(response =>{
-        console.log("Print name",response.employeeUpdateChoice)
+
+        //Updates the employees department
+        console.log(`${response.employeeUpdateDept}, ${response.employeeUpdateName}`)
+        // connection.query(`UPDATE department SET name = ${response.employeeUpdateChoice} where `)
+
+        // console.log("Print name",response.employeeUpdateChoice)
         setSafeModeOff();
-        chosenName = response.employeeUpdateChoice
+        chosenName = response.employeeUpdateName
+        inquirer.prompt([
+
+        ])
+
+
         // return chosenName;
-        console.log("Passed through function?", chosenName)
+        // console.log("Passed through function?", chosenName)
     })
     
         });
-        console.log(employeeNames);
+        // console.log(employeeNames);
     
 
     
